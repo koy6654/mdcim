35af5e8d0266cc97e570a36dc65e59a1453a3a24
koy6654 <koy410994@gmail.com>
2020-10-12 12:01:08 +0900

    DeviceSettings/NetworkServices/HTTP

--- src/screens/DeviceSettings/NetworkServices/HTTP.js
+++ src/screens/DeviceSettings/NetworkServices/HTTP.js
@@ -20,7 +20,7 @@ export default class HTTP extends Component {
             paddingLeft: '10px',
             paddingRight: '10px',
             fontSize: '20px',
-            paddingBottom: '30px',
+            paddingBottom: '15px',
             paddingTop: '25px',
         }
         return (
@@ -29,15 +29,34 @@ export default class HTTP extends Component {
                     <Col>
                         <Box title='HTTP' border type='default' collapsable collapsed solid>
                             <div style={divStyle}>
+                                <div style={{ display: 'inline-block' }}>
+                                    Enable HTTP access
+                                </div>
+                                <div style={{ display: 'inline-block', verticalAlign: 'middle', marginLeft: '299px' }}>
+                                    <input type='checkbox' style={{ width: '23px', height: '23px' }} />
+                                </div>
+                            </div>
+                            <div style={divStyle}>
+                                <div style={{ display: 'inline-block' }}>
+                                    Enforce use of HTTPS
+                                </div>
+                                <div style={{ display: 'inline-block', verticalAlign: 'middle', marginLeft: '288px' }}>
+                                    <input type='checkbox' style={{ width: '23px', height: '23px' }} />
+
+                                </div>
+                                <div style={{ fontSize: '16px', paddingLeft: '30px', display: 'inline-block', verticalAlign: 'middle', }}>Redirect HTTP connections to HTTPS</div>
+                            </div>
+                            <div style={divStyle}>
                                 Port
                                 <input
                                     name='httpport'
                                     value={this.state.inputValue}
                                     onChange={this.handleChange}
                                     onFocus={(oF) => oF.target.placeholder = ''}
-                                    style={{ marginLeft: '287px', width: '1175px', borderRadius: '5px', borderWidth: '1px', padding: '3px', paddingLeft: '10px', borderColor: '#E5E5E5' }}
+                                    style={{ marginLeft: '430px', width: '1175px', borderRadius: '5px', borderWidth: '1px', padding: '3px', paddingLeft: '10px', borderColor: '#E5E5E5' }}
                                     placeholder='required' />
                             </div>
+                            <br />
                         </Box>
                     </Col>
                 </Row>
@@ -46,19 +65,37 @@ export default class HTTP extends Component {
                     <Col>
                         <Box title='HTTPS' border type='default' collapsable collapsed solid>
                             <div style={divStyle}>
-                                First DNS Server
+                                <div style={{ display: 'inline-block' }}>
+                                    Enable HTTPS access
+                                </div>
+                                <div style={{ display: 'inline-block', verticalAlign: 'middle', marginLeft: '288px' }}>
+                                    <input type='checkbox' style={{ width: '23px', height: '23px' }} />
+                                </div>
+                            </div>
+                            <div style={divStyle}>
+                                <div style={{ display: 'inline-block' }}>
+                                    Enable HSTS
+                                </div>
+                                <div style={{ display: 'inline-block', verticalAlign: 'middle', marginLeft: '359px' }}>
+                                    <input type='checkbox' style={{ width: '23px', height: '23px' }} />
+                                </div>
+
+                            </div>
+                            <div style={divStyle}>
+                                Port
                                 <input
                                     name='httpsport'
                                     value={this.state.inputValue}
                                     onChange={this.handleChange}
                                     onFocus={(oF) => oF.target.placeholder = ''}
-                                    style={{ marginLeft: '287px', width: '1175px', borderRadius: '5px', borderWidth: '1px', padding: '3px', paddingLeft: '10px', borderColor: '#E5E5E5' }}
+                                    style={{ marginLeft: '430px', width: '1175px', borderRadius: '5px', borderWidth: '1px', padding: '3px', paddingLeft: '10px', borderColor: '#E5E5E5' }}
                                     placeholder='required' />
                             </div>
+                            <br />
                         </Box>
                     </Col>
                 </Row>
-            </Content>
+            </Content >
         );
     }
 }
