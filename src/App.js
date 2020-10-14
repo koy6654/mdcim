f691a0a7884c995832bc919f7359798f0cc7ba01
koy6654 <koy410994@gmail.com>
2020-10-14 12:03:13 +0900

    add LoginSettings & App either

--- src/App.js
+++ src/App.js
@@ -81,8 +81,8 @@ export default class App extends Component {
         <RoleBasedAccessControl path='/devicesettings/security/rolebasedaccesscontrol' exact />
         <TLSCertificate path='/devicesettings/security/tlscertificate' exact />
         <Authentication path='/devicesettings/security/authentication' exact />
-        {/* <LoginSettings path='/devicesettings/security/loginsettings' exact />
-        <PasswordPolicy path='/devicesettings/security/passwordpolicy' exact />
+        <LoginSettings path='/devicesettings/security/loginsettings' exact />
+        {/* <PasswordPolicy path='/devicesettings/security/passwordpolicy' exact />
         <ServiceAgreement path='/devicesettings/security/serviceagreement' exact /> */}
 
 
